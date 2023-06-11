d6146e6190035e83f8e1ece3a87da45664aa5a49
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-06-11 07:37:50 +0300

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = ''
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            new_string += i
-    return new_string
+    if len(my_string) > 0:
+        str_list = []
+        for let in my_string:
+            if let != 'c' and let != 'C':
+                str_list.append(let)
+        my_string = ''
+        for let in str_list:
+            my_string += let
+        return my_string
