50bc5595e540bf62c33a4841fdeedbdadd0e8997
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-06-11 12:58:14 +0300

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * count - elemnts number of the linked list
+ * @head: head node
+ * Return: number of elements
+ */
+
+int count(listint_t *head)
+{
+	if (head == NULL)
+		return (0);
+
+	return (1 + count(head->next));
+}
+
+/**
+ * is_palindrome -checks if a singly linked list is a palindrome.
+ * @head: head node
+ * Return: 0 if not ot 1 if it is
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int num_of_elemnts = count(*head);
+	int i = 0, f, l, f_data, l_data;
+	listint_t *first, *last;
+
+	if (num_of_elemnts == 0)
+		return (1);
+
+	while (i != num_of_elemnts / 2)
+	{
+		i++;
+		first = *head;
+
+		for (f = 0; f < i; f++)
+		{
+			f_data = first->n;
+			first = first->next;
+		}
+
+		last = *head;
+		for (l = 0; l < num_of_elemnts - (i - 1); l++)
+		{
+			l_data = last->n;
+			last = last->next;
+		}
+
+		if (f_data != l_data)
+			return (0);
+	}
+
+	return (1);
+}
