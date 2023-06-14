0b1c18ff58e6dd5da2833b0551c3b9ffb0b63a60
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-06-14 12:48:41 +0300

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     if len(matrix) > 0:
         new_matrix = []
-        new_matrix = [[r[i] * r[i] for i in range(len(r))] for r in matrix]
+        new_matrix = [[r[i] ** 2 for i in range(len(r))] for r in matrix]
         return new_matrix
