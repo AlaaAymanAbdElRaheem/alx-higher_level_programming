0dba03e6775e504a7637ce3597dd0fb6757b73ee
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-06-14 14:42:00 +0300

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict):
+        return None
+    else:
+        result = 0
+        best_key = ''
+        for k, v in a_dictionary.items():
+            if v > result:
+                result = v
+                best_key = k
+    return best_key
