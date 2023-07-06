c2dea6efd296819de1aec550df7084ad81653834
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-07-06 04:17:27 +0300

    task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,12 +5,11 @@
 def matrix_divided(matrix, div):
     """divides all elements of a matrix"""
     if div is None:
-        raise TypeError("""
-matrix_divided() missing 1 required positional argument: 'div'""")
+        raise TypeError("matrix_divided() missing 1 required positional argument:\
+ 'div'")
     elif matrix is None and div is None:
-        raise TypeError("""
-matrix_divided() missing 2 required positional arguments:
- 'matrix' and 'div'""")
+        raise TypeError("matrix_divided() missing 2 required positional arguments:\
+ 'matrix' and 'div'")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -18,21 +17,21 @@ matrix_divided() missing 2 required positional arguments:
         raise TypeError("div must be a number")
 
     if not isinstance(matrix, list):
-        raise TypeError("""
-matrix must be a matrix (list of lists) of integers/floats""")
+        raise TypeError("matrix must be a matrix (list of lists) of\
+ integers/floats")
 
     row_len = 0
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("""
-matrix must be a matrix (list of lists) of integers/floats""")
+            raise TypeError("matrix must be a matrix (list of lists) of\
+ integers/floats")
         if len(row) != row_len and row_len != 0:
             raise TypeError("Each row of the matrix must have the same size")
         row_len = len(row)
         for x in row:
             if not isinstance(x, (int, float)):
-                raise TypeError("""
-matrix must be a matrix (list of lists) of integers/floats""")
+                raise TypeError("matrix must be a matrix (list of lists) of\
+ integers/floats")
 
     new_mat = [[round(x / div, 2) for x in row] for row in matrix]
     return new_mat
