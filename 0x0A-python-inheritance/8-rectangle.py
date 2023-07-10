33319431a6e882a537a68894f8624bd792dc1576
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-07-10 20:20:17 +0300

    task 7

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,13 +10,17 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validates value"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
 
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/7-base_geometry.txt")
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
