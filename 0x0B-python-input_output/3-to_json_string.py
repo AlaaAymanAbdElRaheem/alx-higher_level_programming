7d35619a94195e575335c8d7db6eb9aa3132920e
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-07-11 16:36:45 +0300

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defining to_json_string function"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """implement to_json_string function"""
+    return json.dumps(my_obj)
