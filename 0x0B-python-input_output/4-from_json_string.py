96ba66f90e0c031031d9769dd68e26ca39482e12
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-07-11 16:41:48 +0300

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defining from_json_string function"""
+
+
+import json
+
+
+def from_json_string(my_obj):
+    """implement from_json_string function"""
+    return json.loads(my_obj)
