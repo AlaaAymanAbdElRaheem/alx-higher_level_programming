d1be9225aca723daa586beeb709e0cdb2daf21d2
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-07-17 07:07:02 +0300

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """defining Base class"""
 
 
+import json
+
+
 class Base:
     """implement Base class"""
     __nb_objects = 0
@@ -13,3 +16,57 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w', encoding='utf-8') as f:
+            if list_objs is None:
+                f.write("[]")
+
+            list_of_dic = []
+            for obj in list_obj:
+                list_of_dic.append(cls.to_dictionary(obj))
+
+            return f.write(cls.to_json_string(list_of_dic))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(2, 2)
+        elif cls.__name__ == "Square":
+            dummy = cls(2)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        filename = cls.__name__ + '.json'
+        list_of_inst = []
+
+        try:
+            with open(filename, 'r') as f:
+                data = cls.from_json_string(f.read())
+            for elm in data:
+                list_of_inst.append(cls.create(**elm))
+        except FileNotFound:
+            pass
+
+        return list_of_inst
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,6 +5,7 @@ of the Base class
 """
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
@@ -25,3 +26,32 @@ class TestBase(unittest.TestCase):
     def test_id_is_None(self):
         """id is None"""
         self.assertTrue(Base(None), self.id == 3)
+
+    def test_to_json_string(self):
+        """returns the JSON string representation of list_dictionaries"""
+        dic = {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}
+        json_dic = Base.to_json_string(dic)
+        self.assertEqual(
+            json_dic, '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]')
+
+    def test_from_json_string(self):
+        """returns the list of the JSON string representation json_string"""
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(list_input, [{'height': 4, 'width': 10, 'id': 89},
+                                      {'height': 7, 'width': 1, 'id': 7}])
+        self.assertEqual(json_list_input,
+                         '[{"height": 4, "width": 10, "id": 89},\
+ {"height": 7, "width": 1, "id": 7}]')
+        self.assertEqual(list_output, [{'height': 4, 'width': 10, 'id': 89},
+                                       {'height': 7, 'width': 1, 'id': 7}])
+
+    def test_create(self):
+        """returns an instance with all attributes already set"""
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual(str(r2), "[Rectangle] (1) 1/0 - 3/5")
