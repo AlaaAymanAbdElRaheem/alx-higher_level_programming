c7d4dd9ee670fff9f7aab36ceb1d86177bf256e5
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-09-13 02:09:24 +0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 
 const baseSquare = require('./5-square.js');
 
-module.exports = class Square extends baseSquare{
+module.exports = class Square extends baseSquare {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
