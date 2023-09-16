ac1c1d960f5b92a4229f77abd4aff032eb550499
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-09-16 13:52:14 +0300

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+import sys
+from urllib.parse import quote_plus
+import sqlalchemy
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    passwd = quote_plus(sys.argv[2])
+    engine = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                                      .format(sys.argv[1],
+                                              passwd, sys.argv[3]),
+                                      pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(engine)
+    session = session()
+    result = session.query(State).order_by(State.id).first()
+
+    if result:
+        print("{}: {}".format(result.id, result.name))
+    else:
+        print("Nothing")
