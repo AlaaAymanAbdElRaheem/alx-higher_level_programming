dccc75c42fdc47a74288f108baa88dbb022eb9c8
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-09-29 02:27:45 +0300

    fixing task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -H "X-School-User-Id: 98" 0.0.0.0:5000/route_5
+curl -s -H "X-School-User-Id: 98" "$1"
