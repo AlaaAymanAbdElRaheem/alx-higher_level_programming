52fd23d9d93255150b3863e8a8d180f69428aa76
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-09-29 01:43:57 +0300

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-res= $(curl -sL -w "%{http_code}" "$1"); stat=$(echo "$res"|cut -d" " -f2); if ["$stat" -eq 200] then; rsult= $(echo "$res" | cut -d" " f1; fi; echo"$result"
+res= $(curl -sL -w "%{http_code}" "$1"); stat=$(echo "$res"|cut -d" " -f2); if ["$stat" -eq 200] then; rsult= $(echo "$res" | cut -d" " f1); fi; echo"$result"
