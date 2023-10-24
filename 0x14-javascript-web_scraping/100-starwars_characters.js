a1c5767d164705c1b134511f631e2e4786839c29
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-10-24 23:53:46 +0300

    task 7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    return console.log(err);
+  }
+
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    request(character, function (err, response, body) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
