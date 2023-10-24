0da3e437a550965f9fceee6b3009bee30cf73a2f
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-10-25 00:13:53 +0300

    fixing task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,24 @@
 #!/usr/bin/node
 
+const { resolve } = require('path');
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url, function (err, response, body) {
+request(url, async function (err, response, body) {
   if (err) {
     return console.log(err);
   }
 
   const characters = JSON.parse(body).characters;
   for (const character of characters) {
-    request(character, function (err, response, body) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(JSON.parse(body).name);
+    await new Promise((resolve, reject) => {
+      request(character, function (err, response, body) {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
   }
 });
